1fa64595d0f5531be0861017b844a69c6fee72f4
GeorgeDV <georgedevries909@hotmail.com>
2022-10-21 16:51:39 +1100

    function getComputerChoice was added in, working

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello World")
+function getComputerChoice(ComputerSelection) {
+
+    if (ComputerSelection == 0)
+        return ("rock")
+    if (ComputerSelection == 1)
+        return ("paper")
+    if (ComputerSelection == 2)
+        return ("scissors")
+
+    console.log(ComputerSelection)
+}
+
+getComputerChoice(Math.floor(Math.random() * 3));
