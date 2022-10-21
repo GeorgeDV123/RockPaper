b7bfb8ab0242ab6e7f74dd7f02e481e834a1a675
GeorgeDV <georgedevries909@hotmail.com>
2022-10-21 17:00:48 +1100

    added playerSelection, working

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 function getComputerChoice(ComputerSelection) {
 
     if (ComputerSelection == 0)
-        return ("rock")
+        return ("rock");
     if (ComputerSelection == 1)
-        return ("paper")
+        return ("paper");
     if (ComputerSelection == 2)
-        return ("scissors")
+        return ("scissors");
 
-    console.log(ComputerSelection)
+    console.log(ComputerSelection);
 }
 
+function playerChoice(playerSelection) {
+
+    if (playerSelection == 1)
+        return ("rock");
+    if (playerSelection == 2)
+        return ("paper");
+    if (playerSelection == 3)
+        return ("scissors")
+
+    console.log(playerSelection)
+}
+
+playerChoice(parseInt(prompt("Enter 1 for Rock, 2 for paper, 3 for scissors")))
 getComputerChoice(Math.floor(Math.random() * 3));
