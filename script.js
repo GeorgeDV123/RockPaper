3e454cedc1cfc1e8be3050c9ee282471efe0fdc8
GeorgeDV <georgedevries909@hotmail.com>
2022-10-22 11:42:10 +1100

    Saving before changing playerchoice

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getComputerChoice(ComputerSelection) {
 
+    ComputerSelection = (Math.floor(Math.random() * 3));
+
     if (ComputerSelection == 0)
         return ("rock");
     if (ComputerSelection == 1)
@@ -12,11 +14,11 @@ function getComputerChoice(ComputerSelection) {
 
 function playerChoice(playerSelection) {
 
-    if (playerSelection == 1)
+    if (playerSelection == 0)
         return ("rock");
-    if (playerSelection == 2)
+    if (playerSelection == 1)
         return ("paper");
-    if (playerSelection == 3)
+    if (playerSelection == 2)
         return ("scissors")
 
     console.log(playerSelection)
@@ -26,6 +28,9 @@ function theGame(ComputerSelection, playerSelection) {
     
 }
 
-
-playerChoice(parseInt(prompt("Enter 1 for Rock, 2 for paper, 3 for scissors")));
+playerChoice(parseInt(prompt("Enter 0 for Rock, 1 for paper, 2 for scissors")));
 getComputerChoice(Math.floor(Math.random() * 3));
+const ComputerSelection = getComputerChoice();
+const playerSelection = playerChoice(parseInt(prompt("Enter 0 for Rock, 1 for paper, 2 for scissors")));
+
+
