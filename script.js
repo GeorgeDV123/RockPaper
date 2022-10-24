bc390c69ae2f8e5c1c4fce9cbc22184a56f01786
GeorgeDV <georgedevries909@hotmail.com>
2022-10-24 14:05:58 +1100

    Starting again on script2.js

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
-function getComputerChoice(ComputerSelection) {
+function getComputerChoice(x) {
 
-    if (ComputerSelection == 0)
+    if (x == 0)
         return ("rock");
-    if (ComputerSelection == 1)
+    else if (x == 1)
         return ("paper");
-    if (ComputerSelection == 2)
+    else if (x == 2)
         return ("scissors");
-
+    
 }
 
-function playRound(playerSelection, ComputerSelection) {
-    ComputerSelection = getComputerChoice(Math.floor(Math.random() * 3));
-    playerSelection = (prompt("Enter rock, paper or scissors"));
+function playRound() {
+   let ComputerSelection = getComputerChoice(Math.floor(Math.random() * 3));
+   let playerSelection = (prompt("Enter rock, paper or scissors"));
 
     if (ComputerSelection == playerSelection)
-        return ("Draw")
+        return ("Draw");
     else if (ComputerSelection == "rock") 
         if (playerSelection == "paper")
-            return ("You Win")
+            return ("You Win");
     else if (ComputerSelection == "paper")
         if (playerSelection == "scissors")
-            return ("You Win")
+            return ("You Win");
     else if (ComputerSelection == "scissors")
         if (playerSelection == "rock")
-            return ("You Win")
+            return ("You Win");
     else if (ComputerSelection == "scissors") 
         if (playerSelection == "paper")
-            return ("You Lose")
+            return ("You Lose");
     else if (ComputerSelection == "paper")
         if (playerSelection == "rock")
-            return ("You Lose")
+            return ("You Lose");
     else if (ComputerSelection == "rock")
         if (playerSelection == "scissors")
-            return ("You Lose")
+            return ("You Lose");
 
 }
 
-playRound()
+function playGame(x, y) {
+    for (let i = 0; i < 5; i++) {
+        if (playRound() = "You Win")
+            x = x++;
+        if (playRound() = "You Lose")
+            y = y++;
+    }
 
+    if (x > y)
+        return("You Won the game!")
+    else
+        return("You Lost the game :(")
+}
+
+playGame()
